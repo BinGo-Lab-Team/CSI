10850be52c86d79b0535c7dc07ac404ecf3f62c0
YanAnghelp <yananghelp@gmail.com>
2025-09-13 01:34:07 +0800

    Fix: 修复流动条边缘白缝；LowDPI问题。Add: 添加超出窗口部分裁切。WRN: 未来需要考虑替换OpacityMask属性

--- main.cpp
+++ main.cpp
@@ -20,62 +20,64 @@
 
 // 日志处理函数
 static void fileMessageHandler(QtMsgType type, const QMessageLogContext& ctx, const QString& msg) {
-    static QFile file("logs/app.log");
-    static QMutex mtx;
-    static bool inited = [] {
-        QDir().mkpath("logs");
-        file.open(QIODevice::Append | QIODevice::Text);
-        return true;
-    }();
+	static QFile file("logs/app.log");
+	static QMutex mtx;
+	static bool inited = [] {
+		QDir().mkpath("logs");
+		file.open(QIODevice::Append | QIODevice::Text);
+		return true;
+		}();
 
-    Q_UNUSED(inited);
-    QString level =
-        type == QtDebugMsg ? "DBG" : type == QtInfoMsg ? "INF" :
-        type == QtWarningMsg ? "WRN" : type == QtCriticalMsg ? "CRT" : "FTL";
+	Q_UNUSED(inited);
+	QString level =
+		type == QtDebugMsg ? "DBG" : type == QtInfoMsg ? "INF" :
+		type == QtWarningMsg ? "WRN" : type == QtCriticalMsg ? "CRT" : "FTL";
 
-    QMutexLocker lock(&mtx);
-    QTextStream out(&file);
-    out << QDateTime::currentDateTime().toString(Qt::ISODateWithMs)
-        << " [" << (quintptr)QThread::currentThreadId() << "] "
-        << level << " " << msg << '\n';
-    out.flush();
+	QMutexLocker lock(&mtx);
+	QTextStream out(&file);
+	out << QDateTime::currentDateTime().toString(Qt::ISODateWithMs)
+		<< " [" << (quintptr)QThread::currentThreadId() << "] "
+		<< level << " " << msg << '\n';
+	out.flush();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-    // 初始化
-    qInstallMessageHandler(fileMessageHandler);         // 接管日志系统
-    QQuickStyle::setStyle("Fusion");                    // 设置默认样式
-    QApplication app(argc, argv);                       // 创建应用程序对象
+	// 初始化
+	qInstallMessageHandler(fileMessageHandler);         // 接管日志系统
+	QQuickStyle::setStyle("Fusion");                    // 设置默认样式
 
-    // 加载字体
-    int id = QFontDatabase::addApplicationFont(":/res/font/AiDianFengYaHei.ttf");
-    if (id != -1) {
-        QString family = QFontDatabase::applicationFontFamilies(id).at(0);
-        QFont font(family);
-        font.setPixelSize(14);  // 默认字号
-        app.setFont(font);      // 设置为全局字体
-    }                           // 若加载成功，更改全局默认字体
+	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication app(argc, argv);                       // 创建应用程序对象
 
-    // 设置窗口图标
-    app.setWindowIcon(QIcon(":/res/icon/main/construction.svg"));
-    QQmlApplicationEngine engine;                   // 创建 QML 引擎对象
-    engine.loadFromModule("csi", "Main");           // 加载 qml 入口文件
+	// 加载字体
+	int id = QFontDatabase::addApplicationFont(":/res/font/AiDianFengYaHei.ttf");
+	if (id != -1) {
+		QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+		QFont font(family);
+		font.setPixelSize(14);  // 默认字号
+		app.setFont(font);      // 设置为全局字体
+	}                           // 若加载成功，更改全局默认字体
 
-    if (!engine.rootObjects().isEmpty()) {
-        auto window = qobject_cast<QQuickWindow*>(engine.rootObjects().first());
-        if (window) {
-            // 必须先设置无边框
-            window->setFlags(Qt::FramelessWindowHint | Qt::Window);
-            // 再设置背景透明
-            window->setColor(Qt::transparent);
-        }
-    }
+	// 设置窗口图标
+	app.setWindowIcon(QIcon(":/res/icon/main/construction.svg"));
+	QQmlApplicationEngine engine;                   // 创建 QML 引擎对象
+	engine.loadFromModule("csi", "Main");           // 加载 qml 入口文件
 
-    // 打印错误
-    QObject::connect(&engine, &QQmlApplicationEngine::warnings,[](const QList<QQmlError>& ws) { for (auto& w : ws) qWarning().noquote() << w.toString(); });
-    if (engine.rootObjects().isEmpty()) return -1;      // 若初始化失败，退出程序
+	if (!engine.rootObjects().isEmpty()) {
+		auto window = qobject_cast<QQuickWindow*>(engine.rootObjects().first());
+		if (window) {
+			// 必须先设置无边框
+			window->setFlags(Qt::FramelessWindowHint | Qt::Window);
+			// 再设置背景透明
+			window->setColor(Qt::transparent);
+		}
+	}
 
-    // 进入事件循环
-    return app.exec();
+	// 打印错误
+	QObject::connect(&engine, &QQmlApplicationEngine::warnings, [](const QList<QQmlError>& ws) { for (auto& w : ws) qWarning().noquote() << w.toString(); });
+	if (engine.rootObjects().isEmpty()) return -1;      // 若初始化失败，退出程序
+
+	// 进入事件循环
+	return app.exec();
 }
